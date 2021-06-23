c3f9f9de3cdb74f2ccc1d5550606f3eef1491fb8
Flavio Spedaletti <flavio.hfs@gmail.com>
2021-06-23 16:54:23 -0300

    alteração flunt

--- src/Investimentos.Service.Api/DTOs/AtivoRendaVariavelDTO.cs
+++ src/Investimentos.Service.Api/DTOs/AtivoRendaVariavelDTO.cs
@@ -1,9 +1,24 @@
-﻿using Investimentos.Service.Api.Enums;
+﻿using Flunt.Validations;
+using Investimentos.Service.Api.Enums;
 
 namespace Investimentos.Service.Api.DTOs
 {
     public class AtivoRendaVariavelDTO : DTOBase
     {
+        public AtivoRendaVariavelDTO(ETipoRendaVariavel tipoRendaVariavel, string nomePregao, string codigoNegociacao)
+        {
+            TipoRendaVariavel = tipoRendaVariavel;
+            NomePregao = nomePregao;
+            CodigoNegociacao = codigoNegociacao;
+
+            AddNotifications(new Contract()
+                 .Requires()
+                 .IsNotNullOrEmpty(nomePregao, "AtivoRendaVariavelDTO.NomePregao", "Nome do pregão é obrigatório")
+                 .HasMaxLen(nomePregao, 100, "AtivoRendaVariavelDTO.NomePregao", "Nome do pregão não pode ter mais que 100 caracteres.")
+                 .IsNotNullOrEmpty(codigoNegociacao, "AtivoRendaVariavelDTO.CodigoNegociacao", "Código de negociação é obrigatório")
+                 .HasMaxLen(codigoNegociacao, 10, "AtivoRendaVariavelDTO.CodigoNegociacao", "Código de negociação não pode ter mais que 10 caracteres.")
+             );
+        }
         //TODO: validação nos DTOs?? Por enquanto herdei a classe Notifiable do Flunt
         public ETipoRendaVariavel TipoRendaVariavel { get; set; }
         public string NomePregao { get; set; }
